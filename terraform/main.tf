5b0d00c9d9973f5606bb7c37cb569b32b5b1ee1b
danishkhantech11 <141706605+danishkhantech11@users.noreply.github.com>
2024-01-05 14:41:37 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+# main.tf
+
+# Define a variable
+variable "example_variable" {
+  type    = string
+  default = "Hello, Terraform!"
+}
+
+# Print the value of the variable using the output block
+output "output_example" {
+  value = var.example_variable
+}
