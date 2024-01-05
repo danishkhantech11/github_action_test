2b0f7ea0a45b0a47d4acef0c29f84125d9812b86
danishkhantech11 <141706605+danishkhantech11@users.noreply.github.com>
2024-01-05 15:53:19 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,29 @@
-# variables.tf
+variable "v1" {
+  default     = 1
+  description = "Initial desired task count for ECS Service"
+  type        = number
+}
 
-# Assign a value to the example_variable
-variable "example_variablessss" {
-  default = "Custom value for example_variable"
+variable "v2" {
+  default     = 1
+  description = "Initial min task count for ECS Service"
+  type        = number
+}
+
+variable "v3" {
+  default     = 6
+  description = "Initial max task count for ECS Service"
+  type        = number
+}
+
+variable "v4" {
+  default     = 512
+  description = "Task CPU setting"
+  type        = number
+}
+
+variable "v5" {
+  default     = 1024
+  description = "Task memory setting"
+  type        = number
 }
