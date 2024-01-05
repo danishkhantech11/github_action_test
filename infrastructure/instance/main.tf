44760fbb70c85a9deda1722544ff96e47d37cf37
danishkhantech11 <141706605+danishkhantech11@users.noreply.github.com>
2024-01-05 18:39:14 +0530

    Update main.tf

--- infrastructure/instance/main.tf
+++ infrastructure/instance/main.tf
@@ -1,3 +1,41 @@
+variable "aspnetcore_image" {
+  description = "Docker image pushed to ECR"
+}
+
+variable "newrelic_license_key" {
+  default = ""
+}
+
+variable "splunk_hec_token" {
+  description = "Optional Splunk HTTP Event Collector token enables Splunk log driver"
+  default     = ""
+}
+
+variable "service_name" {
+  description = "The first part of the name of the service (usually api or www)"
+  default     = "api"
+}
+
+variable "infra_prefix" {
+  description = "The prefix used to create resources in AWS"
+  type = string
+}
+
+variable "secondary_region" {
+  description = "The secondary region available for disaster recovery"
+  default = "us-east-2"
+}
+
+variable "region" {
+  description = "The secondary region available for disaster recovery"
+  default = "us-east-2"
+}
+
+variable "aspnetcore_environment" {
+    description = "Dotnet environment var"
+}
+
+
 output "ecs-infra" {
   value = {
   desired_task_count  = var.secondary_region == var.region ? 0 : var.desired_task_count
