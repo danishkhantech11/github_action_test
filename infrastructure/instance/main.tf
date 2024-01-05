ecd32dd6cde11fe927a6c0aaa3051d54df3c2e73
danishkhantech11 <141706605+danishkhantech11@users.noreply.github.com>
2024-01-05 19:13:07 +0530

    Update main.tf

--- infrastructure/instance/main.tf
+++ infrastructure/instance/main.tf
@@ -1,26 +1,3 @@
-variable "aspnetcore_image" {
-  description = "Docker image pushed to ECR"
-}
-
-variable "newrelic_license_key" {
-  default = ""
-}
-
-variable "splunk_hec_token" {
-  description = "Optional Splunk HTTP Event Collector token enables Splunk log driver"
-  default     = ""
-}
-
-variable "service_name" {
-  description = "The first part of the name of the service (usually api or www)"
-  default     = "api"
-}
-
-variable "infra_prefix" {
-  description = "The prefix used to create resources in AWS"
-  type = string
-}
-
 variable "secondary_region" {
   description = "The secondary region available for disaster recovery"
   default = "us-east-2"
@@ -31,9 +8,7 @@ variable "region" {
   default = "us-east-2"
 }
 
-variable "aspnetcore_environment" {
-    description = "Dotnet environment var"
-}
+
 
 
 
