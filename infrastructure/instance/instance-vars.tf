65f9abcc0b5502e81e7d1e0ceefe6834047da54e
danishkhantech11 <141706605+danishkhantech11@users.noreply.github.com>
2024-01-05 17:30:40 +0530

    Create instance-vars.tf

---
+++ infrastructure/instance/instance-vars.tf
@@ -0,0 +1,29 @@
+variable "desired_task_count" {
+  default     = 1
+  description = "Initial desired task count for ECS Service"
+  type        = number
+}
+
+variable "min_task_count" {
+  default     = 1
+  description = "Initial min task count for ECS Service"
+  type        = number
+}
+
+variable "max_task_count" {
+  default     = 6
+  description = "Initial max task count for ECS Service"
+  type        = number
+}
+
+variable "task_cpu" {
+  default     = 512
+  description = "Task CPU setting"
+  type        = number
+}
+
+variable "task_memory" {
+  default     = 1024
+  description = "Task memory setting"
+  type        = number
+}
