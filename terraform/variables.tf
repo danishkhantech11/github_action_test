b975592cb1d439784b78fd3da5e891b8dc1c085a
danishkhantech11 <141706605+danishkhantech11@users.noreply.github.com>
2024-01-05 15:07:22 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,6 @@
-variable "VMCount" {
-    description = "How many VMs do you want to start with (number)? default=1 max=5"
-    type = number
+# variables.tf
+
+# Assign a value to the example_variable
+variable "example_variable" {
+  default = "Custom value for example_variable"
 }
