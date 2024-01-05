f4dfeb55707d6155a9dc09230f3a196ff627ac2b
danishkhantech11 <141706605+danishkhantech11@users.noreply.github.com>
2024-01-05 16:15:53 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,10 @@ variable "example_variable" {
 }
 
 # Print the value of the variable using the output block
-output "output_example" {
+output "output_example1" {
   value = var.v1
 }
 
-output "output_example" {
+output "output_example2" {
   value = var.v2
 }
